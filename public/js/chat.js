d3358b6051053a033e3be2b9de3d795510a2a8c4
jose-abel <guest1@guest1s-MacBook-Pro.local>
2022-08-16 15:10:18 -0400

    Add the funcionality to scroll down

--- public/js/chat.js
+++ public/js/chat.js
@@ -16,6 +16,29 @@ const sidebarTemplate = document.getElementById('sidebar-template').innerHTML;
 // Options
 const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true });
 
+const autoscroll = () => {
+	// New message element
+	const $newMessage = $messages.lastElementChild;
+
+	// Height of the new message
+	const newMessageStyles = getComputedStyle($newMessage);
+	const newMessageMargin = parseInt(newMessageStyles.marginBottom);
+	const newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
+
+	// Visible height
+	const visibleHeight = $messages.offsetHeight;
+
+	// Height of messages container
+	const containerHeight = $messages.scrollHeight;
+
+	// How far have I scrolled>
+	const scrollOffset = $messages.scrollTop + visibleHeight;
+
+	if (containerHeight - newMessageHeight <= scrollOffset) {
+		$messages.scrollTop = $messages.scrollHeight;
+	}
+	
+}
 
 socket.on("message", message => {
 	const html = Mustache.render(messageTemplate, {
@@ -25,6 +48,7 @@ socket.on("message", message => {
 	});
 
 	$messages.insertAdjacentHTML('beforeend', html);
+	autoscroll();
 });
 
 socket.on("locationMessage", (message) => {
@@ -35,6 +59,7 @@ socket.on("locationMessage", (message) => {
 	});
 
 	$messages.insertAdjacentHTML('beforeend', html);
+	autoscroll();
 });
 
 socket.on("roomData", ({ room, users }) => {
